389d8cf11e0310192c812331dc815c0e56a63cc1
user2 <user2@simlab20>
2019-09-05 20:10:16 +0530

    area

---
+++ src/Calculator/Shape.java
@@ -0,0 +1,14 @@
+package Calculator;
+
+public abstract class Shape {
+    protected double area;
+
+    public double getArea() {
+        return area;
+    }
+
+    protected abstract void onAreaChange();
+
+	
+
+}
