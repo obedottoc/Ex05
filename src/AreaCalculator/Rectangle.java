0a657ce3289cebd33ecadd35633f3d1f16debcfe
user2 <user2@administrator-HP-Pro-3330-MT>
2019-09-12 12:19:04 +0530

    5

---
+++ src/AreaCalculator/Rectangle.java
@@ -0,0 +1,15 @@
+package AreaCalculator;
+import java.util.Scanner;
+
+public class Rectangle extends Shape{
+    Double Rarea;
+    Scanner sc =new Scanner(System.in);
+    void printarea() {
+        System.out.println("Enter the height and breadth of Rectangle: ");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        Rarea=(double) (a*b);               
+        System.out.println("The area of Reactangle is: "+Rarea);
+    }
+           
+}
