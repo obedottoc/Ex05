3c7eeaf85c16c1df38752bcedd113eb929971040
user3 <user3@simlab26>
2019-09-16 19:02:05 +0530

    this program is to print area

---
+++ src/shapeclass/Shape.java
@@ -0,0 +1,7 @@
+package shapeclass;
+
+public abstract class Shape {
+	protected int a;
+	protected int b;
+abstract public void printArea();
+}
---
+++ src/shapeclass/Triangle.java
@@ -0,0 +1,23 @@
+package shapeclass;
+
+import java.util.Scanner;
+
+public class Triangle extends Shape {
+public Triangle(int h,int b) {
+	super.a=h;
+	super.b=b;
+}
+public void getinput() {
+	 Scanner sc=new Scanner(System.in);
+	 System.out.println("For the Area of Triangle Enter the height and base");
+	 a=sc.nextInt();
+	 b=sc.nextInt();
+	
+}
+
+public void  printArea() {
+	 double Areaoftriangle;
+	 Areaoftriangle=0.5*a*b;
+	 System.out.println("The area of Triangle is"+Areaoftriangle);
+}
+}
