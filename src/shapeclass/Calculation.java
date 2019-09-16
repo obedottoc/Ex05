bacaada807f34eca60e2f4c42308a27c3801179a
user3 <user3@simlab23>
2019-09-16 20:26:19 +0530

    to develop a application for area calculator

--- src/shapeclass/Calculation.java
+++ src/shapeclass/Calculation.java
@@ -6,7 +6,6 @@ public static void main(String[]args) {
 	Rectangle r=new Rectangle(12,5);
 	r.printArea();
 	Triangle t=new Triangle(45,6);
-	t.getinput();
 	t.printArea();
 	Circle c=new Circle(5);
 	c.printArea();
--- src/shapeclass/Triangle.java
+++ src/shapeclass/Triangle.java
@@ -7,13 +7,6 @@ public Triangle(int h,int b) {
 	super.a=h;
 	super.b=b;
 }
-public void getinput() {
-	 Scanner sc=new Scanner(System.in);
-	 System.out.println("For the Area of Triangle Enter the height and base");
-	 a=sc.nextInt();
-	 b=sc.nextInt();
-	
-}
 
 public void  printArea() {
 	 double Areaoftriangle;
